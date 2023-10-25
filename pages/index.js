a1c46aa93933778c8efc33b1d8ff0dc28e5e04c5
SPhotography <110682198+SPhotography@users.noreply.github.com>
2023-10-25 12:52:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,52 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="bg">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Онлайн магазин</title>
+</head>
+<body>
+    <header>
+        <h1>Добре дошли в нашия онлайн магазин</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Начало</a></li>
+                <li><a href="#">Продукти</a></li>
+                <li><a href="#">Количка</a></li>
+                <li><a href="#">Контакти</a></li>
+            </ul>
+        </nav>
+    </header>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <section>
+        <h2>Продукти</h2>
+        <ul>
+            <li>
+                <img src="product1.jpg" alt="Продукт 1">
+                <h3>Продукт 1</h3>
+                <p>Описание на продукт 1.</p>
+                <p>Цена: $50.00</p>
+                <button>Добави в количката</button>
+            </li>
+            <li>
+                <img src="product2.jpg" alt="Продукт 2">
+                <h3>Продукт 2</h3>
+                <p>Описание на продукт 2.</p>
+                <p>Цена: $40.00</p>
+                <button>Добави в количката</button>
+            </li>
+        </ul>
+    </section>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    <section>
+        <h2>Количка</h2>
+        <ul>
+            <!-- Тук ще бъдат показани продуктите, добавени в количката -->
+        </ul>
+    </section>
 
-      <Footer />
-    </div>
-  )
-}
+    <footer>
+        <p>&copy; 2023 Вашият Онлайн Магазин</p>
+    </footer>
+</body>
+</html>
